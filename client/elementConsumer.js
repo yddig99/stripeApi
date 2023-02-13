8d6ac480a11bbef75405ae60e39c59da6f93d7b4
yddig99 <gideonkwake99@gmail.com>
2023-02-13 16:57:14 +0300

    stripeApi classedbased

---
+++ client/elementConsumer.js
@@ -0,0 +1,41 @@
+import {ElementsConsumer , PaymentElement} from '@stripe/react-stripe-js'
+
+class CheckoutForm extends React.Component {
+    handleSubmit = async (e) => {
+        e.preventDefault();
+        const {stripe ,elements}= this.props ;
+        if (!stripe||!elements) {
+            return;
+        }
+        const result = await stripe.confirmPayment({
+           elements,
+           confirmParams:{
+            return_url:"",
+           }, 
+        });
+
+        if (result.error) {
+            console.log(result.error.message);
+        }else{
+            //your customer
+        }
+    };
+    render(){
+        return(
+            <form onSubmit ={this.handleSubmit}>
+                <PaymentElement/>
+                <button disabled ={!this.props.stripe}>Submit</button>
+            </form>
+        );
+    }
+}
+
+export default function InjectedCheckoutForm(){
+    return(
+        <ElementsConsumer>
+            {({stripe , elements}) =>(
+                <CheckoutForm stripe = {stripe} elements={elements}/>
+            )}
+        </ElementsConsumer>
+    )
+}
